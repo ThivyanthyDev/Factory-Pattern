3f04a7946042f9844881870a3f60e0fad93ac6ca
ThivyanthyDev <devdhivy@gmail.com>
2021-01-08 21:02:43 +0530

    Add project files.

---
+++ Factory Pattern/Add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Pattern.Factory_Pattern
+{
+    class Add : ICalculate
+    {
+        public void Calculate(double a, double b)
+        {
+            Console.WriteLine("a + b is {0}", a + b);
+        }
+    }
+}
---
+++ Factory Pattern/Test.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Pattern.Factory_Pattern
+{
+    class Test
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter first number");
+            string input = Console.ReadLine();
+            double num1, num2;
+            bool result = Double.TryParse(input, out num1);
+            if (!result)
+            {
+                Console.WriteLine("Please enter a number");
+                return;
+            }
+            Console.WriteLine("Enter second number");
+            result = Double.TryParse(Console.ReadLine(), out num2);
+            if (!result)
+            {
+                Console.WriteLine("Please enter a number");
+                return;
+            }
+
+            Console.WriteLine("Enter Add, Subtract or Divide");
+            CalculateFactory factory = new CalculateFactory();
+            ICalculate obj = factory.GetCalculation(Console.ReadLine());
+            //Divide obj = new Divide();
+            obj.Calculate(num1, num2);
+            Main();
+
+        }
+    }
+}
